693382f486417e1845862e9963071cda9c776fc8
aidanliddiard <aidanliddiard@gmail.com>
2022-06-14 12:18:10 -0700

    books displaying and book test passing

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -1,6 +1,6 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const { request } = require('supertest');
+const request = require('supertest');
 const app = require('../lib/app');
 
 describe('books routes', () => {
@@ -11,7 +11,8 @@ describe('books routes', () => {
   it('should return a list of books with publication year', async () => {
     const res = await request(app).get('/books');
     expect(res.body.length).toEqual(16);
-    const wild = res.body.find((char) => char.id === '1');
+    const wild = res.body.find((book) => book.id === '1');
+    expect(wild).toHaveProperty('title', 'Where the Wild Things Are');
     expect(wild).toHaveProperty('released', 1963);
   });
 
